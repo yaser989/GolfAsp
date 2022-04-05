bd58e8989f20ef08a679176c806a10faf51772a6
Yaser ABBAS <y.abbas@groupeonepoint.com>
2022-04-05 09:58:00 +0200

    first commit

---
+++ golf/golf/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace golf
+{
+    partial class Golf
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.Gol = new System.Windows.Forms.PictureBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.forcetxt = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Gol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AllowDrop = true;
+            this.txtScore.Font = new System.Drawing.Font("Consolas", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.Black;
+            this.txtScore.Location = new System.Drawing.Point(424, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(306, 151);
+            this.txtScore.TabIndex = 0;
+            this.txtScore.Text = "Score: 0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.txtScore.UseMnemonic = false;
+            // 
+            // Gol
+            // 
+            this.Gol.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Gol.Location = new System.Drawing.Point(327, 69);
+            this.Gol.Name = "Gol";
+            this.Gol.Size = new System.Drawing.Size(41, 42);
+            this.Gol.TabIndex = 2;
+            this.Gol.TabStop = false;
+            this.Gol.Tag = "gol";
+            this.Gol.Click += new System.EventHandler(this.Gol_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.FloralWhite;
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ball.Location = new System.Drawing.Point(336, 550);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(17, 22);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            this.ball.Tag = "ball";
+            this.ball.Click += new System.EventHandler(this.ball_Click);
+            // 
+            // forcetxt
+            // 
+            this.forcetxt.AutoSize = true;
+            this.forcetxt.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.forcetxt.Location = new System.Drawing.Point(292, 588);
+            this.forcetxt.Name = "forcetxt";
+            this.forcetxt.Size = new System.Drawing.Size(120, 33);
+            this.forcetxt.TabIndex = 3;
+            this.forcetxt.Tag = "forcetxt";
+            this.forcetxt.Text = "force: ";
+            this.forcetxt.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Golf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(739, 630);
+            this.Controls.Add(this.forcetxt);
+            this.Controls.Add(this.Gol);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.txtScore);
+            this.Name = "Golf";
+            this.Text = "Golf";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Gol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.PictureBox Gol;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Label forcetxt;
+    }
+}
+
---
+++ golf/golf/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace golf
+{
+    public partial class Golf : Form
+    {
+
+       
+        bool clickSpasse, isGameOver, shotingBall ;
+
+       
+        int score;
+
+        int BallSpeed = 60;
+
+
+        int force = 100;
+        int forceSpeed = 2;
+
+
+        public Golf()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Gol_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+
+        }
+
+   
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+
+            txtScore.Text = "Score: " + score;
+
+            forcetxt.Text = "Force: " + force;
+
+
+            force -= forceSpeed;
+
+            if (force < 0 || force > 100)
+            {
+                forceSpeed = -forceSpeed;
+            }
+
+
+
+            if (clickSpasse == true && force > 50)
+            {
+               
+                ball.Top -= 200;
+                score++;
+                clickSpasse = false;
+               
+                
+            }
+            else if (clickSpasse == true && force < 50)
+            {
+
+                ball.Top -= 30;
+                score++;
+                clickSpasse = false;
+               
+
+            }
+            else if (clickSpasse == true && force == 0)
+            {
+                clickSpasse = false;
+            }
+          
+
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+
+                    if ((string)x.Tag == "gol")
+                    {
+                        if (ball.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            GameTimer.Stop();
+                            isGameOver = true;
+                            txtScore.Text = "Score: " + score + Environment.NewLine + "you win!";
+                            ball.Visible = false;
+                            
+                        }
+                    }
+
+
+                }
+            }
+          
+           
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+         
+
+            if(e.KeyCode == Keys.Space)
+            {
+                clickSpasse = true;
+
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+
+            if (e.KeyCode == Keys.Space)
+            {
+                clickSpasse = false;
+                
+            }
+
+            if (e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                RestartGame();
+            }
+
+        }
+
+        private void RestartGame()
+        {
+            clickSpasse = false;
+            isGameOver = false;
+            shotingBall = false;
+      
+            score = 0;
+           
+
+            txtScore.Text = "Score: " + score;
+
+            forcetxt.Text = "Force: " + force;
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox &&  x.Visible == false)
+                {
+                    x.Visible = true;
+                }
+            }
+
+            // reset the position of the game
+            ball.Location = new Point((ball.Parent.ClientSize.Width / 2) - (ball.Width / 2),
+                                  (ball.Parent.ClientSize.Height / 2) - (ball.Height / 2)) ;
+            ball.Refresh();
+
+
+            GameTimer.Start();
+
+
+        }
+
+            }
+
+        
+
+    
+}
